cad1b72dbf0cbd3fea0be59494348d5795f3b34f
cnsr <dmytrenkoyar@gmail.com>
2017-05-24 23:53:47 +0300

    added everything

---
+++ config.py
@@ -0,0 +1,13 @@
+Trip = '' #tripcodem without '#'
+nolimitCookie = 'annacookie'
+name = "" # desired username
+user_id = # int of userid, given by telegram to your user
+
+#token of telegram bot
+token = ""
+
+#you can use those
+weatherapikeys = [
+'2239cd846f2b4993143a4596a6566d5d',
+'0f091d195880f217476122810828159d',
+'8cd355d522adede0682f891077502794']
---
+++ users_dict.py
@@ -0,0 +1,9 @@
+#users = {'id':'nickname'}
+users = {
+'$2a$10$mAM0oYrjp0bCHFDsGaiB.ebDctpgMPeOJKhaioCsXECeJj7JCNf9m':'TnT',
+'$2a$10$mAM0oYrjp0bCHFDsGaiB.erXc97WCLWDReWPUKUG8Dlo24qIDXCNu':'kebek',
+'$2a$10$mAM0oYrjp0bCHFDsGaiB.ekC6gZM23YHU7zVasPKeGWAD2aj40vjS':'ekb',
+'$2a$10$mAM0oYrjp0bCHFDsGaiB.eu7KdIQ7zPuXJHb24qFGNtr5xTB..lMm':'me',
+'$2a$10$mAM0oYrjp0bCHFDsGaiB.eqjmBs2nRa.L0uNgjgULovUXc2RFZlG6':'woop',
+'$2a$10$mAM0oYrjp0bCHFDsGaiB.eXOjQL5cW0vJaYbRBkUjZQQ6zrE7gTmi':'sadfrogs'
+}
