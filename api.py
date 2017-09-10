df2a6c1c73b531f821c883b45d9bb6b01d4678e3
cnsr <dmytrenkoyar@gmail.com>
2017-09-11 02:29:59 +0300

    slightly refactored according to pep-8, added ogg support and now bot avoids video files

--- api.py
+++ api.py
@@ -10,34 +10,38 @@ import config
 cookies = cookielib.LWPCookieJar()
 
 handlers = [
-	urllib2.HTTPHandler(),
-	urllib2.HTTPSHandler(),
-	urllib2.HTTPCookieProcessor(cookies)
+    urllib2.HTTPHandler(),
+    urllib2.HTTPSHandler(),
+    urllib2.HTTPCookieProcessor(cookies)
 ]
 
 opener = poster.streaminghttp.register_openers()
 for handler in handlers:
-	opener.add_handler(handler)
-	
+    opener.add_handler(handler)
+
 curr_chat_room = None
 socketIO = None
 mesg_queue = []
 
+
 def fetch(uri):
-	req = urllib2.Request(uri)
-	return opener.open(req)
-	
-def post(uri,params):
-	datagen, headers = poster.encode.multipart_encode(params)
-	req = urllib2.Request(uri,datagen,headers)
-	return opener.open(req)
-	
+    req = urllib2.Request(uri)
+    return opener.open(req)
+
+
+def post(uri, params):
+    datagen, headers = poster.encode.multipart_encode(params)
+    req = urllib2.Request(uri, datagen, headers)
+    return opener.open(req)
+
+
 def get_password():
     for cookie in cookies:
         if cookie.name == "password_livechan":
             cookie.value = config.nolimitCookie
             return cookie.value
     return ""
+
 
 def post_chat(body, chat, name=config.name, convo="General", trip="", file="" ):
     post_params = []
@@ -57,23 +61,27 @@ def post_chat(body, chat, name=config.name, convo="General", trip="", file="" ):
     req = urllib2.Request(uri, datagen, headers)
     return opener.open(req)
 
+
 def join_chat(chat_room):
     global curr_chat_room
     global socketIO
     if (curr_chat_room != None):
-        socketIO.emit('unsubscribe', curr_chat_room);
-    socketIO.emit('subscribe', chat_room);
+        socketIO.emit('unsubscribe', curr_chat_room)
+    socketIO.emit('subscribe', chat_room)
+
 
 def display_chat(chat_obj):
     print(chat_obj["name"])
     print(chat_obj["body"])
     print
 
+
 def get_data(chat):
     data_response = fetch('https://kotchan.org/data/'+chat)
     json_data = json.loads(data_response.read())
     for i in json_data[::-1]:
         display_chat(i)
+
 
 def main_chat(chat_room):
     chat_body = raw_input("> ")
@@ -82,29 +90,32 @@ def main_chat(chat_room):
     mainresp = post_chat(chat_body, chat_room)
     return False
 
+
 def on_chat(*args):
-    if (args[0]["name"] == "IRCBot"):
+    if args[0]["name"] == "IRCBot":
         return
     msg = args[0]["name"]+"~ "
-    if ("image" in args[0]):
+    if "image" in args[0]:
         filename = "https://kotchan.org/tmp/uploads/"+re.compile('[\w\-\.]*$').search(args[0]["image"]).group(0)
         msg += "file: "+filename+" "
     msg += (" ".join(args[0]["body"].splitlines()))
     livechanBot.sendMsg(channel, msg)
 
+
 def on_user_count(*args):
     print(args[0], "users online")
-    print
+
 
 def on_request_location(*args):
     global chat_room
     socketIO.emit('subscribe', curr_chat_room);
 
+
 def login(callback=on_chat):
     cookie = cookielib.Cookie(version=0, name='password_livechan', value=config.nolimitCookie, port=None, port_specified=False, domain='kotchan.org',
-            domain_specified=False, domain_initial_dot=False,
-            path='/', path_specified=True, secure=False, expires=None,
-            discard=True, comment=None, comment_url=None, rest={'HttpOnly': None}, rfc2109=False)
+                              domain_specified=False, domain_initial_dot=False,
+                              path='/', path_specified=True, secure=False, expires=None,
+                              discard=True, comment=None, comment_url=None, rest={'HttpOnly': None}, rfc2109=False)
     cookies.set_cookie(cookie)
     livechan_pass = get_password()
     if livechan_pass == "":
@@ -113,9 +124,8 @@ def login(callback=on_chat):
 
     global socketIO
     socketIO = SocketIO('https://kotchan.org',
-        cookies={'password_livechan': livechan_pass})
+                        cookies={'password_livechan': livechan_pass})
     socketIO.on('chat', callback)
     socketIO.on('request_location', on_request_location)
     
-    thread.start_new_thread ( socketIO.wait, () )
- 
+    thread.start_new_thread(socketIO.wait, ())
--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
-#remade by cnsr based on livechan API and anna bot code
-#added telegram integration
-###bot starts receiving incoming data only after sending any outcoming message
-#kinda works but not really
+# remade by cnsr based on livechan API and anna bot code
+# added telegram integration
+# bot starts receiving incoming data only after sending any outcoming message
+# kinda works but not really
 
-#imports
+# imports
 from time import sleep
 from api import *
+import os
 import telebot
 import re
 import sys
@@ -15,195 +16,216 @@ from urllib2 import urlopen
 import hbot
 import requests
 import pyowm
-
-import users_dict
-
 import urllib
 from bs4 import BeautifulSoup, Comment
 
 tbot = telebot.TeleBot(config.token)
+nsfw = False
 
-#if you don't input channel to connect to'
-if (len(sys.argv) < 2):
-	print("Usage: python bot.py [channel]")
-	exit()
+# if you don't input channel to connect to
+if len(sys.argv) < 2:
+    print("Usage: python bot.py [channel]")
+    exit()
 
-#if you did it just werks
+# if you did it just werks
 channel = sys.argv[1]
 
-#writes image on disk so it could be readen by api later
-def send_image(url):
-	url = url.replace('/home/ph/livechan-js/public/',
-	'https://kotchan.org/')
-	f = open('out.jpg','wb')
-	f.write(urllib2.urlopen(url).read())
-	f.close()
+# if nswf doesnt send the images
+try:
+    if sys.argv[2] == 'nsfw':
+        nsfw = True
+        print('NSFW is on!')
+except IndexError:
+    print('NSFW is off!')
 
-#used to check if user exists in separate file so you could add him if he doesnt
-def write_in(txt,txt1,txt2):
-	with open('users.txt','a') as f:
-		f.write('\n' + txt + ' - ' + txt1 + ' - ' + txt2)
+
+# writes image on disk so it could be read by api later
+def send_image(url):
+    url = url.replace('/home/ph/livechan-js/public/',
+                      'https://kotchan.org/')
+    f = open('out.jpg', 'wb')
+    f.write(urllib2.urlopen(url).read())
+    f.close()
+
+
+def send_ogg(url):
+    url = url.replace('/home/ph/livechan-js/public/',
+                      'https://kotchan.org/')
+    f = open('out.ogg', 'wb')
+    f.write(urllib2.urlopen(url).read())
+    f.close()
+
+
+# used to check if user exists in separate file so you could add him if he doesnt
 
 base_url = 'http://www.worldtimeserver.com/current_time_in_'
 
+
 class Opener(urllib.FancyURLopener):
-	version = 'App/1.7'
+    version = 'App/1.7'
+
 
 def process_chat(*args):
-	#uncomment for debug:
-	#print(args)
-	#just to ease finding this block in code
-	try:
-		#get vars from args
-		ident = args[0]["identifier"]
-		message = args[0]["body"]
-		name = args[0]["name"]
-		count = str(args[0]["count"])
-		convo = args[0]["convo"]
-		country_name = args[0]["country_name"]
-		country = args[0]["country"]
-		
-		#print(country)	
-		#print(ident)
-		
-		def get_time():
-			c = ''
-			result = ''
-			result_time = ''
-			for char in country:
-				if char.isalpha():
-					c += char
-			c = c[:2]
-			url = base_url + c + '.aspx'	
-			r = Opener().open(url)
-			soup = BeautifulSoup(r,'lxml')
-			#gets time
-			time_comments = soup.findAll(text=lambda text:isinstance(text,Comment))
-			for x in time_comments:
-				y = ''
-				for char in x:
-					if char != ' ':
-						y += char
-				if re.match('ServerTimewithseconds:',y):
-					result = y
-			for char in result:
-				if not char.isalpha():
-					result_time += char
-			result_time = result_time[1:]
-			#gets city + country
-			city_found = soup.find('h1',{'class':'placeNameH1'})
-			city = city_found.text
-			#cleaning up city output
-			city = re.sub('\s+','',city)
-			city = ' '.join(re.findall('[A-Z][^A-Z]*',city))
-			#prints result
-			return ('Time in {0} is {1}'.format(city, result_time))
-		
-		
-		help_msg = 'no help message defined'
-		
-		#for those who don't have names
-		if ident not in users_dict.users:
-			name += " | " + country
-		
-		#gets weather, sometimes off
-		wreq = re.compile('\@weather( (.+))?').match(message)
-		if wreq:
-			answer = ''
-			try:
-				wr = wreq.group(2)
-				# setting up weather
-				random.shuffle(config.weatherapikeys)
-				apikey = random.choice(config.weatherapikeys)
-				owm = pyowm.OWM(apikey)
-				obs = owm.weather_at_place(wr)
-				w = obs.get_weather()
-				#gets all variables
-				location = obs.get_location()
-				loc = location.get_name()
-				tempC = w.get_temperature('celsius')
-				tempF = w.get_temperature('fahrenheit')
-				tC = str(tempC['temp'])
-				tF = str(tempF['temp'])
-				hum = str(w.get_humidity())
-				status = w.get_detailed_status()
-				answer = ('Weather in {0}:\ntemperature: {1}C, {2}F\n\
-humidity: {3}%\nweather: {4}').format(loc, tC, tF, hum, status)
-			except Exception as e:
-				print(e)
-				answer = 'error occured while parsing your request, check spelling and make sure you entered location'
-			out_msg = '>>' + count + '\n' + answer
-			post_chat(out_msg, channel, name = config.name,trip = config.Trip,convo = 'General', file = '')
-		
-		#gets time
-		#THIS DOESNT FUCKING WORK IN A SEPARATE FILE
-		#if re.match(message,'.htime') and message !='':
-		#	out_msg = out_msg = '>>' + count + '\n' + get_time()
-		#	post_chat(out_msg, channel, name = config.name,trip = config.Trip,convo = 'General', file = '')
-		
-		#bot commands
-		#YES STEF THIS IS THE WAY IT SHOULD FUCKING BE NO FUCKING CALLBACK FUNCS
-		for (k,v) in hbot.answers.iteritems():
-			if re.match(k,message):
-				help_msg = hbot.answers[k]
-				out_msg = '>>' + count + '\n' + help_msg
-				post_chat(out_msg, channel, name = config.name,trip = config.Trip,convo = 'General', file = '')
-				
-		#handles text messages only, text + photo will only be handled as photo by next handler
-		@tbot.message_handler(func=lambda incM: True)
-		def handle_text(incM):
-			post_chat(incM.text, channel, name = config.name,trip = config.Trip, convo="General",file = '')
-		
-		#only handles photos, doesnt work with text
-		@tbot.message_handler(content_types=['photo','text'])
-		def handle_image(message):
-			file_id = message.photo[-1].file_id
-			imageIn = tbot.get_file(file_id)
-			image_file = requests.get('https://api.telegram.org/file/bot' + config.token + '/' + imageIn.file_path)
-			with open('in.jpg','wb') as f:
-				f.write(image_file.content)
-			post_chat('',channel, name=config.name,trip = config.Trip, convo = 'General', file = 'in.jpg')
-		
-		#only sends posts from 'General' conversation
-		if convo == "General":
-			if "image" in args[0].keys():
-				out_image = args[0]["image"]
-			else:
-				out_image = ''
-			
-			msg = str(count) + '| ' +  name + ":\n" + message
-			
-			#this doesnt work smh
-			try:
-				subname = config.name.split('#');
-				if name != config.name and name != subname[0]:
-					if out_image != '':
-						send_image(out_image)
-						img = open('out.jpg', 'rb')
-						tbot.send_photo(config.user_id, img)
-						img.close()
-					tbot.send_message(config.user_id, msg)
-					# print(msg)
-			except Exception as e:
-				ree = '----------\n' + str(e) + '\n-----------'
-				tbot.send_message(config.user_id, ree)
-	except Exception as e:
-		print('Exception' + str(e))
-#		print(e)
-		
-#i guess this is better to have than not to?		
-try:	
-	login(callback=process_chat)
-	join_chat(channel)
-	print('Joined chat')
-	#init msg to make bot work
-	#it doesnt smh lel
-	tbot.send_message(config.user_id, 'Joined chat.')
-except Exception as e:
-	print("Connection failed. Error message:")
-	print(e)
+    # uncomment for debug:
+    # print(args)
+    # just to ease finding this block in code
+    try:
+        # get vars from args
+        ident = args[0]["identifier"]
+        message = args[0]["body"]
+        name = args[0]["name"]
+        count = str(args[0]["count"])
+        convo = args[0]["convo"]
+        country_name = args[0]["country_name"]
+        country = args[0]["country"]
+        if "image" in args[0].keys():
+            extension = os.path.splitext(args[0]["image"])[-1].lower()
+        else:
+            extension = ''
+        # print(country)
+        # print(ident)
 
-#makes bot work and tbot polls endlessly(at least while no errors occure)
+        def get_time():
+            c = ''
+            result = ''
+            result_time = ''
+            for char in country:
+                if char.isalpha():
+                    c += char
+            c = c[:2]
+            url = base_url + c + '.aspx'
+            r = Opener().open(url)
+            soup = BeautifulSoup(r, 'lxml')
+            # gets time
+            time_comments = soup.findAll(text=lambda text: isinstance(text, Comment))
+            for x in time_comments:
+                y = ''
+                for char in x:
+                    if char != ' ':
+                        y += char
+                if re.match('ServerTimewithseconds:', y):
+                    result = y
+            for char in result:
+                if not char.isalpha():
+                    result_time += char
+            result_time = result_time[1:]
+            # gets city + country
+            city_found = soup.find('h1', {'class': 'placeNameH1'})
+            city = city_found.text
+            # cleaning up city output
+            city = re.sub('\s+', '', city)
+            city = ' '.join(re.findall('[A-Z][^A-Z]*', city))
+            # prints result
+            return 'Time in {0} is {1}'.format(city, result_time)
+
+        help_msg = 'no help message defined'
+
+        # gets weather, sometimes off
+        wreq = re.compile('\@weather( (.+))?').match(message)
+        if wreq:
+            answer = ''
+            try:
+                wr = wreq.group(2)
+                # setting up weather
+                random.shuffle(config.weatherapikeys)
+                apikey = random.choice(config.weatherapikeys)
+                owm = pyowm.OWM(apikey)
+                obs = owm.weather_at_place(wr)
+                w = obs.get_weather()
+                # gets all variables
+                location = obs.get_location()
+                loc = location.get_name()
+                tempC = w.get_temperature('celsius')
+                tempF = w.get_temperature('fahrenheit')
+                tC = str(tempC['temp'])
+                tF = str(tempF['temp'])
+                hum = str(w.get_humidity())
+                status = w.get_detailed_status()
+                answer = ('Weather in {0}:\ntemperature: {1}C, {2}F\n\
+                humidity: {3}%\nweather: {4}').format(loc, tC, tF, hum, status)
+            except Exception as e:
+                print(e)
+                answer = 'error occured while parsing your request, check spelling and make sure you entered location'
+            out_msg = '>>' + count + '\n' + answer
+            post_chat(out_msg, channel, name=config.name, trip=config.Trip,
+                      convo='General', file='')
+
+        # gets time
+        # THIS DOESNT FUCKING WORK IN A SEPARATE FILE
+        # if re.match(message,'.htime') and message !='':
+        # out_msg = out_msg = '>>' + count + '\n' + get_time()
+        # post_chat(out_msg, channel, name = config.name,trip = config.Trip, convo = 'General', file = '')
+
+        # bot commands
+        # YES STEF THIS IS THE WAY IT SHOULD FUCKING BE NO FUCKING CALLBACK FUNCS
+        for (k, v) in hbot.answers.iteritems():
+            if re.match(k, message):
+                help_msg = hbot.answers[k]
+                out_msg = '>>' + count + '\n' + help_msg
+                post_chat(out_msg, channel, name=config.name, trip=config.Trip,
+                          convo='General', file='')
+
+        # handles text messages only, text + photo will only be handled as photo by next handler
+        @tbot.message_handler(func=lambda incM: True)
+        def handle_text(incM):
+            post_chat(incM.text, channel, name=config.name, trip=config.Trip,
+                      convo="General", file='')
+
+        # only handles photos, doesnt work with text
+        @tbot.message_handler(content_types=['photo', 'text'])
+        def handle_image(message):
+            file_id = message.photo[-1].file_id
+            imageIn = tbot.get_file(file_id)
+            image_file = requests.get('https://api.telegram.org/file/bot' + config.token + '/' + imageIn.file_path)
+            with open('in.jpg', 'wb') as f:
+                f.write(image_file.content)
+            post_chat('', channel, name=config.name,
+                      trip=config.Trip, convo='General', file='in.jpg')
+
+        # only sends posts from 'General' conversation
+        if convo == "General":
+            if "image" in args[0].keys():
+                out_image = args[0]["image"]
+            else:
+                out_image = ''
+
+            msg = str(count) + '| ' + name + ' | ' + country + " :\n" + message
+
+            # this doesnt work smh
+            try:
+                tbot.send_message(config.user_id, msg)
+                if out_image != '' and not nsfw and extension != '':
+                    if extension not in ['.webm', '.gif', '.ogg', '.mp3', '.mp4']:
+                        send_image(out_image)
+                        img = open('out.jpg', 'rb')
+                        tbot.send_photo(config.user_id, img)
+                        img.close()
+                    if extension == '.ogg':
+                        send_ogg(out_image)
+                        ogg = open('out.ogg', 'rb')
+                        tbot.send_voice(config.user_id, ogg)
+                        ogg.close()
+                # print(msg)
+            except Exception as e:
+                ree = '----------\n' + str(e) + '\n-----------'
+                tbot.send_message(config.user_id, ree)
+    except Exception as e2:
+        print('Exception' + str(e2))
+        # print(e2)
+
+# i guess this is better to have than not to?
+try:	
+    login(callback=process_chat)
+    join_chat(channel)
+    print('Joined chat')
+    # init msg to make bot work
+    # it doesnt smh lel
+    tbot.send_message(config.user_id, 'Joined chat.')
+except Exception as e:
+    print("Connection failed. Error message:")
+    print(e)
+
+# makes bot work and tbot polls endlessly(at least while no errors occur)
 while 1:
-	sleep(3)
-	tbot.polling(none_stop = True)
+    sleep(3)
+    tbot.polling(none_stop=True)
